2e6a042640f141721e7c085fc3a04c409e1a2bf1
Mahdi <mahdiata1390@gmail.com>
2026-08-29 22:32:10 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,98 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { I18nProvider } from "@/i18n";
+import { AuthProvider } from "@/contexts/AuthContext";
+import { AppShell } from "@/components/layout/AppShell";
+import { ProtectedRoute } from "@/components/layout/ProtectedRoute";
+
+import Home from "@/pages/Home";
+import Players from "@/pages/Players";
+import PlayerProfile from "@/pages/PlayerProfile";
+import Duels from "@/pages/Duels";
+import Messages from "@/pages/Messages";
+import Friends from "@/pages/Friends";
+import Leaderboard from "@/pages/Leaderboard";
+import Profile from "@/pages/Profile";
+import Settings from "@/pages/Settings";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
+import ResetPassword from "@/pages/ResetPassword";
+import Notifications from "@/pages/Notifications";
+import NotFound from "@/pages/NotFound";
+
+export default function App() {
   return (
-    <div>
-      COD Duel Hub
-    </div>
+    <I18nProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppShell />}>
+              <Route index element={<Home />} />
+              <Route path="players" element={<Players />} />
+              <Route path="players/:id" element={<PlayerProfile />} />
+              <Route path="leaderboard" element={<Leaderboard />} />
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+              <Route path="reset-password" element={<ResetPassword />} />
+
+              <Route
+                path="duels"
+                element={
+                  <ProtectedRoute>
+                    <Duels />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="messages"
+                element={
+                  <ProtectedRoute>
+                    <Messages />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="friends"
+                element={
+                  <ProtectedRoute>
+                    <Friends />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="profile"
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="settings"
+                element={
+                  <ProtectedRoute>
+                    <Settings />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="notifications"
+                element={
+                  <ProtectedRoute>
+                    <Notifications />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </I18nProvider>
   );
 }
-
-export default App;
